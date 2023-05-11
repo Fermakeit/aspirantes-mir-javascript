d1ac6a01ffc9847389aa015e3dd81dfdc18f8b40
Fernando HernandezR <ferhr.educativo@gmail.com>
2023-05-11 14:02:26 -0500

    solucion a los ejercicios de js

---
+++ hello.js
@@ -0,0 +1,13 @@
+// Fernando L. Hernandez R.
+
+// Función para saludar
+
+function hola(msge){
+    return console.log("hola ",nom, " !!!")
+}
+
+// Asignación de valor a la variable
+nom = "Fernando";
+
+// Invocación de la función
+hola(nom);
